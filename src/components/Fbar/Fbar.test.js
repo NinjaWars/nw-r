a69806788362e9bf5b502c1fe1f58f4d8fb30cfe
Roy R <tchalvakspam@gmail.com>
2018-11-20 20:41:17 -0500

    Add test for Fbar component.

---
+++ src/components/Fbar/Fbar.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { default as Fbar } from './Fbar';
+
+it('renders a footer bar', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Fbar />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
