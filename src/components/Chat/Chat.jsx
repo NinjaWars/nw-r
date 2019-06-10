005021eb28ce60ef7335350a20307518d5518235
Roy R <tchalvakspam@gmail.com>
2019-06-09 21:54:12 -0400

    Update chat styling.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
-import styles from './chat.module.css'
-import ChatMessage from './ChatMessage'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+
+import ChatMessage from './ChatMessage'
+import styles from './chat.module.css'
 
 /**
  * An overall Chat display
  * @param {*} props
  */
-const Chat = ({ classes, className, children, chats, ...rest }) => {
+const Chat = ({ classes, className, children, chats, onClick, ...rest }) => {
     return (
         <div className={cx(className, styles.chat)} {...rest}>
-            <form>
-                <TextField label='Send Message' />
-                <Button type='submit'>Send</Button>
-            </form>
+            <div className={styles['send-area']}>
+                <TextField name="chat-message" className={styles['message-field']} label='Message' />
+                <Button type='submit' className={styles.send} onClick={onClick}>Send</Button>
+            </div>
             {chats.map((chat)=>(
-                <ChatMessage key={chat.id} by={chat.by} datetime={chat.datetime}>{chat.message}</ChatMessage>
-
+                <ChatMessage className={styles['chat-message']} key={chat.id} by={chat.by} datetime={chat.datetime}>{chat.message}</ChatMessage>
             ))}
         </div>
     )
@@ -33,6 +33,8 @@ Chat.propTypes = {
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node
     ]),
+    chats: PropTypes.array.isRequired,
+    onClick: PropTypes.func,
 }
 
 Chat.defaultProps = {
