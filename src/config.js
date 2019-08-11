18c7f8091437fab6bee85205e3edfcffea06b79d
Roy R <tchalvakspam@gmail.com>
2019-08-11 15:54:14 -0400

    Update config to use https nw site.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     apiurl: 'https://doesnotexist.execute-api.us-east-2.amazonaws.com/prod',
-    remote: 'http://www.ninjawars.net/',
+    remote: 'https://www.ninjawars.net/',
 }
