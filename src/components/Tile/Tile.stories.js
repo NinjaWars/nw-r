06735f9d12c0feeea1e35d88f49c9699fcb9a5ac
Roy R <tchalvakspam@gmail.com>
2019-08-20 18:27:28 -0400

    Tile useState removed as unused var.

--- src/components/Tile/Tile.stories.js
+++ src/components/Tile/Tile.stories.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react'
+import React from 'react'
 
 import { storiesOf, addDecorator } from '@storybook/react'
 import CssBaseline from '@material-ui/core/CssBaseline'
