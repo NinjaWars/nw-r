097964d79245490e1a663ec1b18fe05bf84d26dd
Roy R <tchalvakspam@gmail.com>
2019-09-05 03:45:24 -0400

    Add Dark tile CBot story.

--- src/components/CBot/CBot.stories.js
+++ src/components/CBot/CBot.stories.js
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 // eslint-disable-next-line no-unused-vars
 import styles from '../../stories/story.module.css'
 import CBot from '../../components/CBot/CBot'
+import Tile from '../../components/Tile/Tile'
 
 addDecorator(storyFn => <CssBaseline>{storyFn()}</CssBaseline>)
 
@@ -14,4 +15,9 @@ storiesOf('CBot', module)
         <>
             <CBot placeholder='Give me a command...'/>
         </>
+    ))
+    .add('in dark tile', ()=>(
+        <Tile theme='dark'>
+            <CBot placeholder='command...'/>
+        </Tile>
     ))
