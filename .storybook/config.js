c884bf4b8414570abe21820c26c06eb132b3567b
Roy Ronalds <tchalvak.s.pam@gmail.com>
2020-03-30 15:08:29 -0400

    Refactor various stories,
    add withKnobs decorator to stories by default.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { withKnobs } from "@storybook/addon-knobs";
+
+addDecorator(withKnobs)
 
 const loaderFn = () => {
     const allExports = [require('../src/stories/index.js')];
