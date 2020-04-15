b4cfea03754d7fc6f9d5c0bfec3cad9149cc820b
Roy Ronalds <tchalvak.s.pam@gmail.com>
2020-04-14 21:15:02 -0400

    Minor cleanup of Layout stories.

--- src/components/Layout/Layout.stories.js
+++ src/components/Layout/Layout.stories.js
@@ -1,10 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
-
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
-
 // eslint-disable-next-line no-unused-vars
 import styles from '../../stories/story.module.css'
 import { BrowserRouter } from 'react-router-dom'
